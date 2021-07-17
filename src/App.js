7ebc242c45b1b34c088374b8098b73d3017b4730
aridarsan <aridarsan09@gmail.com>
2021-07-17 16:52:39 +0700

    add library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import AutoTyping, { BlinkCursor } from 'react-auto-typing'
 
 function App() {
   return (
@@ -11,12 +12,27 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://wa.me/6281222288153"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Whatsapp me
         </a>
+        <br/>
+        <h1 style={{color: "green"}}>
+        <AutoTyping
+          active // <boolean>
+          textRef='Ari Darsan' // <string>
+          writeSpeed={150} // <number>
+          deleteSpeed={150} // <number>
+          delayToWrite={1000} // <number>
+          delayToDelete={2000} // <number>
+        />
+        <BlinkCursor
+          active // <boolean>
+          blinkSpeed={500} // <number>
+        />
+        </h1>
       </header>
     </div>
   );
