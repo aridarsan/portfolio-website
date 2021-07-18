bcc2feec9498bbc7e60f782c8cd2ac0a93646823
aridarsan <aridarsan09@gmail.com>
2021-07-18 21:15:02 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
-import AutoTyping, { BlinkCursor } from 'react-auto-typing'
+import { Route, Switch } from 'react-router-dom';
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://wa.me/6281222288153"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Whatsapp me
-        </a>
-        <br/>
-        <h1 style={{color: "green"}}>
-        <AutoTyping
-          active // <boolean>
-          textRef='Ari Darsan' // <string>
-          writeSpeed={150} // <number>
-          deleteSpeed={150} // <number>
-          delayToWrite={1000} // <number>
-          delayToDelete={2000} // <number>
-        />
-        <BlinkCursor
-          active // <boolean>
-          blinkSpeed={500} // <number>
-        />
-        </h1>
-      </header>
-    </div>
+    <React.Fragment>
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+      </Switch>
+
+    </React.Fragment>
   );
 }
 
